53e9812e6ec31ecd0e87127ea3f3ce5890f0b86d
Asmit_Shukl <asmitshukla9794@gmail.com>
2026-03-27 17:46:45 +0530

    added authentication and learned importance between relying on middleware and how trpc helps in authentication and how it is supperior

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,21 @@
-import { cn } from "@/lib/utils";
-import { getQueryClient, trpc } from "@/trpc/servere";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import Client from "./client";
 
- const Page=async ()=>{
-  const queryClient=getQueryClient();
+import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/servere";
+import { LogoutButton } from "./logout";
 
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+ const Page= async ()=>{
+  await requireAuth();
+
+  const data =await caller.getUsers();
 
   return (
-    //wht we re doing here is we re creating  boundry between the client component and the server 
-    //component thsi is is very important becuse this is how we usd to write the industry level code
 
-    <div className={cn("text-red-500 min-h-screen min-w-screen flex items-center justify-center")}>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Client/>
-      </HydrationBoundary>
+    <div className={"text-red-500 min-h-screen min-w-screen flex items-center justify-center"}>
+      protected server component
+      {JSON.stringify(data)};
+      <LogoutButton/>
     </div>
   )
 }
 
-export default Page;
+export default Page; 
